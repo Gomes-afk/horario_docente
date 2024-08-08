d5fe3a9818fc89e9fbee630a832871bdba179786
Aluno <Aluno@ETEJK-LAB03-M10>
2024-08-07 21:48:59 -0300

    inclusão do rodape

---
+++ horario_docente/src/main/java/br/com/horario_docente/HorarioDocenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.horario_docente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorarioDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorarioDocenteApplication.class, args);
+	}
+
+}
---
+++ horario_docente/src/main/java/br/com/horario_docente/controller/LoginController.java
@@ -0,0 +1,21 @@
+package br.com.horario_docente.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/") //nome que eu quiser colocar
+	public String priincipal()
+	{
+		return "principal"; //caminho real do arquivo
+	
+	}
+@GetMapping("/principal") //nome que eu quiser colocar
+public String home()
+{
+	return "principal"; //caminho real do arquivo
+}
+
+}
