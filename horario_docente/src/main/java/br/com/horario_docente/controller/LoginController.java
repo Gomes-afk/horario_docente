79085152257933a8c5fa8f4221aa517d71789e05
Aluno <Aluno@ETEJK-LAB03-M05>
2024-08-21 21:40:38 -0300

    Mudança no docente

--- horario_docente/src/main/java/br/com/horario_docente/controller/LoginController.java
+++ horario_docente/src/main/java/br/com/horario_docente/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 
 	@GetMapping("/") //nome que eu quiser colocar
-	public String priincipal()
+	public String principal()
 	{
 		return "principal"; //caminho real do arquivo
 	
@@ -17,5 +17,10 @@ public String home()
 {
 	return "principal"; //caminho real do arquivo
 }
+@GetMapping("/docente") //nome que eu quiser colocar
+public String docente()
+{
+	return "docente"; //caminho real do arquivo
 
+}
 }
