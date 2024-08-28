efec26ad553cad286ad1b9a7113f5c710d0115af
Aluno <Aluno@ETEJK-LAB03-M05>
2024-08-27 21:30:53 -0300

    Classe e atributo

---
+++ horario_docente/src/main/java/br/com/horario_docente/controller/DocenteController.java
@@ -0,0 +1,38 @@
+package br.com.horario_docente.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.horario_docente.entity.DocenteEntity;
+
+@Controller
+public class DocenteController{
+
+
+@GetMapping("/docente") //nome que eu quiser colocar
+public String docente()
+{
+	return "docente"; //caminho real do arquivo
+}
+
+
+
+@PostMapping("/salvar_docente")
+public ModelAndView save (
+		ModelMap model,
+		@ModelAttribute("docenteEntity") DocenteEntity docenteEntity,
+		RedirectAttributes atributes) throws Exception
+{
+System.out.println("Nome :" + docenteEntity.getNome());
+ModelAndView mv = new ModelAndView("redirect:/docente");
+/*atributes.addFlashAttribute("mensagem", docenteService.save(docenteEntity));
+*/
+return mv;
+
+}
+}
